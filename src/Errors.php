cb0233e3e02c05cfe2c9c934bd745366edb804ac
erfan.gooneh@zoodfood.ir <erfan.gooneh@zoodfood.ir>
2024-06-12 19:38:22 +0330

    closed #8 adding most known errors

--- src/Errors.php
+++ src/Errors.php
@@ -2,14 +2,60 @@
 namespace App;
 
 class Errors{
-    public static function NotFound():void{
+
+    public static function InvalidInput(string $message):void{
         header('Content-type: application/json');
-        http_response_code(404);
+        http_response_code(400);
         $response = [
-            "status" => "ERROR",
-            "description"=> "NotFound",
+            "message" => $message
         ];
         echo json_encode($response);
         die();
     }
+    public static function Unauthorized(string $message):void{
+        header('Content-type: application/json');
+        http_response_code(401);
+        $response = [
+            "message" => $message
+        ];
+        echo json_encode($response);
+        die();
+    }
+    public static function Forbidden(string $message):void{
+        header('Content-type: application/json');
+        http_response_code(403);
+        $response = [
+            "message" => $message
+        ];
+        echo json_encode($response);
+        die();
+    }
+    public static function NotFound(string $message):void{
+        header('Content-type: application/json');
+        http_response_code(404);
+        $response = [
+            "message" => $message
+        ];
+        echo json_encode($response);
+        die();
+    }
+    public static function Conflict(string $message):void{
+        header('Content-type: application/json');
+        http_response_code(409);
+        $response = [
+            "message" => $message
+        ];
+        echo json_encode($response);
+        die();
+    }
+    public static function ServerError(string $message):void{
+        header('Content-type: application/json');
+        http_response_code(500);
+        $response = [
+            "message" => $message
+        ];
+        echo json_encode($response);
+        die();
+    }
+    
 }
