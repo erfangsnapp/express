2cde21dbe0cdee5bcf9b5ad89b2aa466858cda33
erfan.gooneh@zoodfood.ir <erfan.gooneh@zoodfood.ir>
2024-06-14 02:22:22 +0330

    adding location distance calculator

---
+++ src/Location.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+class Location{
+    public static function Distance(
+        $latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000):float
+    {
+        // convert from degrees to radians
+        $latFrom = deg2rad($latitudeFrom);
+        $lonFrom = deg2rad($longitudeFrom);
+        $latTo = deg2rad($latitudeTo);
+        $lonTo = deg2rad($longitudeTo);
+
+        $latDelta = $latTo - $latFrom;
+        $lonDelta = $lonTo - $lonFrom;
+
+        $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+                cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+        return abs($angle * $earthRadius);
+    }
+}
