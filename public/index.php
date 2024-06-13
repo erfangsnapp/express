f843501b4ccfdafc0493137096c900fd81410544
erfan.gooneh@zoodfood.ir <erfan.gooneh@zoodfood.ir>
2024-06-14 00:31:18 +0330

    fixing endpoints and turningoff php warnings

--- public/index.php
+++ public/index.php
@@ -3,7 +3,14 @@
 require '../vendor/autoload.php';
 use App\Controllers\HealthController;
 use App\Controllers\BikerController;
+use App\Controllers\TripController;
 use App\Application;
+
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
+ini_set('error_log', 'php://stderr');
+error_reporting(E_ALL);
+
 $config = [
     'db' => [
         'servername' => 'db',
@@ -20,7 +27,10 @@ $router->setRoute('/health',HealthController::class, 'index');
 /////////////////
 
 $router->setRoute('/api/v1/biker/{bikerId}', BikerController::class, 'index');
-
+$router->setRoute('/api/v1/trip/{tripId}', TripController::class, 'get');
+$router->setRoute('/api/v1/trip', TripController::class, 'create');
+$router->setRoute('/api/v1/trip/{tripId}/assign', TripController::class, 'assign');
+$router->setRoute('/api/v1/trip/{tripId}/status', TripController::class, 'status');
 
 $router->run();
 
