09711098c72dc27f2684c17b7ceb0255271f3c77
erfan.gooneh@zoodfood.ir <erfan.gooneh@zoodfood.ir>
2024-06-13 03:48:10 +0330

    Adding dynamic url routing with in-path parametrs feature

--- src/Router.php
+++ src/Router.php
@@ -4,16 +4,25 @@ namespace App;
 class Router{
     protected array $routes = [];
     public function setRoute($route, $controller, $action):void{
+        //REGEX for inpath parameters
+        $route = preg_replace('/{([^}]+)}/', '(?P<$1>[^/]+)', $route);
+        $route = '#^' . $route . '$#';
         $this->routes[$route] = ['controller'=>$controller, 'action'=>$action];
     }
 
     public function run():void{
         $uri = strtok($_SERVER['REQUEST_URI'], '?') ;
-        if(!array_key_exists($uri, $this->routes)) {
-            Errors::NotFound();
+        foreach ($this->routes as $route => $data) {
+            if (preg_match($route, $uri, $matches)) {
+                $controller = new $data['controller']();
+                $action = $data['action'];
+                // Extract named parameters
+                $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
+                $controller->$action($params);
+                return;
+            }
         }
-        $controller = new $this->routes[$uri]['controller']() ;
-        $action =  $this->routes[$uri]['action'];
-        $controller->$action();
+
+        Errors::NotFound();
     }
 }
