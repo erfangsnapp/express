bd81db57610883c9b05e68e0b3d24df2a13750c0
erfan.gooneh@zoodfood.ir <erfan.gooneh@zoodfood.ir>
2024-06-14 12:59:38 +0330

    adding vendors routers

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require '../vendor/autoload.php';
 use App\Controllers\HealthController;
 use App\Controllers\BikerController;
 use App\Controllers\TripController;
+use App\Controllers\VendorController;
 use App\Application;
 
 ini_set('display_errors', '0');
@@ -31,6 +32,8 @@ $router->setRoute('/api/v1/trip/{tripId}', TripController::class, 'get');
 $router->setRoute('/api/v1/trip', TripController::class, 'create');
 $router->setRoute('/api/v1/trip/{tripId}/assign', TripController::class, 'assign');
 $router->setRoute('/api/v1/trip/{tripId}/status', TripController::class, 'status');
+$router->setRoute('/api/v1/vendor', VendorController::class, 'create');
+$router->setRoute('/api/v1/vendor/{vendorId}', VendorController::class, 'get');
 
 $router->run();
 
