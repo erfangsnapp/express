e66d37c20c20b646a3f55036af7abc6e3544ab5d
erfan.gooneh@zoodfood.ir <erfan.gooneh@zoodfood.ir>
2024-06-21 21:37:21 +0330

    Cleaning Trip Service and Controller

--- src/Controllers/TripController.php
+++ src/Controllers/TripController.php
@@ -5,122 +5,155 @@ use App\Response;
 use App\Errors;
 use App\Models\Trip;
 use App\Field;
+
 class TripController{
     public function get($params):void{
         if($_SERVER['REQUEST_METHOD'] == 'GET') {
-            try{
-                $trip_id = $params['tripId'];
-                $trip_field = new Field(Trip::$fieldRules['id'], $trip_id, Trip::class, 'id');
-                $trip_field->validate();
-            }
-            catch (\Throwable $e) {
-                Errors::InvalidInput($e->getMessage());
-            }
-            try {
-                $res = TripService::getTrip($trip_id);
-                Response::JsonResponse($res);
-            } catch (\Exception $e) {
-                if ($e->getCode() == 404) {
-                    Errors::NotFound($e->getMessage());
-                } else {
-                    Errors::ServerError($e->getMessage());
-                }
+            $trip_id = $this->tryToGetTripId($params);
+            $serialized_trip = $this->tryToRetrieveTrip($trip_id);
+            Response::JsonResponse($serialized_trip);
+        }
+    }
+    private function tryToGetTripId($params) {
+        try{
+            $trip_id = $params['tripId'];
+        }
+        catch (\Throwable $e) {
+            Errors::InvalidInput($e->getMessage());
+        }
+        return $trip_id;
+    }
+    private function tryToRetrieveTrip($trip_id) {
+        try {
+            return TripService::getTrip($trip_id);
+        } catch (\Exception $e) {
+            if ($e->getCode() == 404) {
+                Errors::NotFound($e->getMessage());
+            } else {
+                Errors::ServerError($e->getMessage());
             }
         }
     }
+
     public function create():void{
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
-                try{
-                    $data = json_decode(file_get_contents('php://input'), true);
-                    $vendor_id = $data['vendor_id'];
-                    $origin_latitude = $data['origin_latitude'];
-                    $origin_longitude = $data['origin_longitude'];
-                    $destination_latitude = $data['destination_latitude'];
-                    $destination_longitude = $data['destination_longitude'];
-                    $vendor_id_field = new Field(Trip::$fieldRules['vendor_id'], $vendor_id, Trip::class, 'vendor_id');
-                    $origin_latitude_field = new Field(Trip::$fieldRules['origin_latitude'], $origin_latitude, Trip::class, 'origin_latitude');
-                    $origin_longitude_field = new Field(Trip::$fieldRules['origin_longitude'], $origin_longitude, Trip::class, 'origin_longitude');
-                    $destination_latitude_field = new Field(Trip::$fieldRules['destination_latitude'], $destination_latitude, Trip::class, 'destination_latitude');
-                    $destination_longitude_field = new Field(Trip::$fieldRules['destination_longitude'], $destination_longitude, Trip::class, 'destination_longitude');
-                    $vendor_id_field->validate();
-                    $origin_latitude_field->validate();
-                    $origin_longitude_field->validate();
-                    $destination_latitude_field->validate();
-                    $destination_longitude_field->validate();
-                }
-                catch(\Throwable $e){
-                    Errors::InvalidInput($e->getMessage());
-                }
-                try{
-                    $res = TripService::createTrip($vendor_id, $origin_latitude, $origin_longitude, $destination_latitude, $destination_longitude);
-                    Response::JsonResponse($res, 201);
-                }
-                catch(\Exception $e){
-                    if($e->getCode() == 400){
-                        Errors::InvalidInput($e->getMessage());
-                    }
-                    else{
-                        Errors::ServerError($e->getMessage());
-                    }
-                }
-            }
+            list($vendor_id,
+                $origin_latitude,
+                $origin_longitude,
+                $destination_latitude,
+                $destination_longitude
+                ) = $this->tryToGetTripDataFromInput();
+            $serialized_created_trip = $this->tryToCreateTrip(
+                $vendor_id,
+                $origin_latitude,
+                $origin_longitude,
+                $destination_latitude,
+                $destination_longitude
+            );
+            Response::JsonResponse($serialized_created_trip, 201);
+        }
     }
-    public function assign($params):void{
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            try{
-                $data = json_decode(file_get_contents('php://input'), true);
-                $trip_id = $params['tripId'];
-                $biker_id = $data['biker_id'];
-                $trip_id_field = new Field(Trip::$fieldRules['id'], $trip_id, Trip::class, 'id');
-                $biker_id_field = new Field(Trip::$fieldRules['biker_id'], $biker_id, Trip::class, 'biker_id');
-                $trip_id_field->validate();
-                $biker_id_field->validate();
-            }
-            catch(\Throwable $e){
+
+    private function tryToGetTripDataFromInput() {
+        try{
+            $data = json_decode(file_get_contents('php://input'), true);
+            $vendor_id = $data['vendor_id'];
+            $origin_latitude = $data['origin_latitude'];
+            $origin_longitude = $data['origin_longitude'];
+            $destination_latitude = $data['destination_latitude'];
+            $destination_longitude = $data['destination_longitude'];
+            return [$vendor_id, $origin_latitude, $origin_longitude, $destination_latitude, $destination_longitude];
+        }
+        catch(\Throwable $e){
+            Errors::BadRequest($e->getMessage());
+        }
+    }
+
+    private function tryToCreateTrip($vendor_id, $origin_latitude, $origin_longitude, $destination_latitude, $destination_longitude) {
+        try{
+            return TripService::createTrip($vendor_id, $origin_latitude, $origin_longitude, $destination_latitude, $destination_longitude);
+        }
+        catch(\Exception $e){
+            if($e->getCode() == 400){
                 Errors::InvalidInput($e->getMessage());
             }
-            try{
-                $res = TripService::assignTrip($trip_id, $biker_id);
-                Response::JsonResponse($res);
-            }
-            catch(\Exception $e){
-                if($e->getCode() == 400){
-                    Errors::InvalidInput($e->getMessage());
-                }
-                else if($e->getCode() == 409){
-                    Errors::Conflict($e->getMessage());
-                }
-                else{
-                    Errors::ServerError($e->getMessage());
-                }
+            else{
+                Errors::ServerError($e->getMessage());
             }
         }
     }
-    public function status($params):void{
-        if($_SERVER['REQUEST_METHOD'] == 'PUT'){
-            try{
-                $data = json_decode(file_get_contents('php://input'), true);
-                $trip_id = $params['tripId'];
-                $status = $data['status'];
-                $trip_id_field = new Field(Trip::$fieldRules['id'], $trip_id, Trip::class, 'id');
-                $status_field = new Field(Trip::$fieldRules['status'], $status, Trip::class, 'status');
-                $trip_id_field->validate();
-                $status_field->validate();
-            }
-            catch(\Throwable $e){
+
+    public function assign($params):void{
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            list($trip_id, $biker_id) = $this->tryToGetAssignDataFromInput($params);
+            $serialized_assigned_trip = $this->tryToAssignTrip($trip_id, $biker_id);
+            Response::JsonResponse($serialized_assigned_trip);
+        }
+    }
+
+    private function tryToGetAssignDataFromInput($params) {
+        try{
+            $data = json_decode(file_get_contents('php://input'), true);
+            $trip_id = $params['tripId'];
+            $biker_id = $data['biker_id'];
+            $trip_id_field = new Field(Trip::$fieldRules['id'], $trip_id, Trip::class, 'id');
+            $biker_id_field = new Field(Trip::$fieldRules['biker_id'], $biker_id, Trip::class, 'biker_id');
+            $trip_id_field->validate();
+            $biker_id_field->validate();
+            return [$trip_id, $biker_id];
+        }
+        catch(\Throwable $e){
+            Errors::InvalidInput($e->getMessage());
+        }
+    }
+
+    private function tryToAssignTrip($trip_id, $biker_id) {
+        try{
+            return TripService::assignTrip($trip_id, $biker_id);
+        }
+        catch(\Exception $e){
+            if($e->getCode() == 400){
                 Errors::InvalidInput($e->getMessage());
             }
-            try{
-                $res = TripService::updateStatus($trip_id, $status);
-                Response::JsonResponse($res);
+            else if($e->getCode() == 409){
+                Errors::Conflict($e->getMessage());
             }
-            catch(\Exception $e){
-                if($e->getCode() == 400){
-                    Errors::InvalidInput($e->getMessage());
-                }
-                else{
-                    Errors::ServerError($e->getMessage());
-                }
+            else{
+                Errors::ServerError($e->getMessage());
+            }
+        }
+    }
+
+    public function status($params):void{
+        if($_SERVER['REQUEST_METHOD'] == 'PUT'){
+            list($trip_id, $status) = $this->tryToGetStatusDataFromInput($params);
+            $serialized_updated_trip = $this->tryToUpdateStatus($trip_id, $status);
+            Response::JsonResponse($serialized_updated_trip);
+        }
+    }
+
+    private function tryToGetStatusDataFromInput($params) {
+        try{
+            $data = json_decode(file_get_contents('php://input'), true);
+            $trip_id = $params['tripId'];
+            $status = $data['status'];
+            return [$trip_id, $status];
+        }
+        catch(\Throwable $e){
+            Errors::InvalidInput($e->getMessage());
+        }
+    }
+
+    private function tryToUpdateStatus($trip_id, $status) {
+        try{
+            return TripService::updateStatus($trip_id, $status);
+        }
+        catch(\Exception $e){
+            if($e->getCode() == 400){
+                Errors::InvalidInput($e->getMessage());
+            }
+            else{
+                Errors::ServerError($e->getMessage());
             }
         }
     }
--- src/Services/TripService.php
+++ src/Services/TripService.php
@@ -6,103 +6,97 @@ use App\Models\Biker;
 use App\Location;
 class TripService{
     public static function getTrip($trip_id):array{
-        $trip = new Trip();
-        $trip = $trip->getById($trip_id);
-        if ($trip == null) {
-            throw new \Exception("Trip not found", 404);
-        }
+        $trip = Trip::getById_Or404($trip_id);
         return $trip->exportData();
     }
     public static function createTrip($vendor_id, $origin_latitude, $origin_longitude, $destination_latitude, $destination_longitude):array{
         $trip = new Trip();
-        try{
-            $trip->insertData([
-                'vendor_id' => $vendor_id,
-                'origin_latitude' => $origin_latitude,
-                'origin_longitude' => $origin_longitude,
-                'destination_latitude' => $destination_latitude,
-                'destination_longitude' => $destination_longitude,
-                'status' => 'requested'
-            ], Trip::$fieldRules);
-        }
-        catch (\Exception $e) {
-            throw new \Exception($e->getMessage(), 400);
-        }
-
-        try {
-            $trip->create();
-        }
-        catch (\Exception $e) {
-            throw new \Exception("Failed to create trip", 500);
-        }
+        $trip->insertData([
+            'vendor_id' => $vendor_id,
+            'origin_latitude' => $origin_latitude,
+            'origin_longitude' => $origin_longitude,
+            'destination_latitude' => $destination_latitude,
+            'destination_longitude' => $destination_longitude,
+            'status' => 'requested'
+        ]);
+        $trip->create();
         return $trip->exportData();
     }
 
+    const MAX_DISTANCE_FROM_ORIGIN_TO_START_TRIP = 5000;
     public static function assignTrip($trip_id, $biker_id):array{
-        $trip = new Trip();
-        $trip = $trip->getById($trip_id);
-        $biker = new Biker();
-        $biker = $biker->getById($biker_id);
-        if ($trip == null) {
-            throw new \Exception("Trip not found", 404);
-        }
-        if ($biker == null) {
-            throw new \Exception("Biker not found", 404);
-        }
+        $trip = Trip::getById_Or404($trip_id);
+        $biker = Biker::getById_Or404($biker_id);
+
         if ($trip->get_status() != 'requested') {
             throw new \Exception("Trip is not in requested status", 409);
         }
-        $fromLat = $biker->get_location()['latitude'];
-        $fromLong = $biker->get_location()['longitude'];
-        $toLat = $trip->get_origin()['latitude'];
-        $toLong = $trip->get_origin()['longitude'];
-        if (Location::Distance($fromLat, $fromLong, $toLat, $toLong) > 5000) {
+
+        $bikerLatitude = $biker->get_location()['latitude'];
+        $bikerLongitude = $biker->get_location()['longitude'];
+        $originLatitude = $trip->get_origin()['latitude'];
+        $originLongitude = $trip->get_origin()['longitude'];
+        if (
+            Location::Distance(
+                $bikerLatitude,
+                $bikerLongitude,
+                $originLatitude,
+                $originLongitude) > self::MAX_DISTANCE_FROM_ORIGIN_TO_START_TRIP) {
             throw new \Exception("Biker is too far", 409);
         }
+
         $trip->insertData([
             'biker_id' => $biker_id,
             'status' => 'assigned'
-        ], Trip::$fieldRules);
-        try {
+        ]);
             $trip->save();
-        }
-        catch (\Exception $e) {
-            throw new \Exception("Failed to assign trip", 500);
-        }
         return $trip->exportData();
     }
 
+    const MAX_DISTANCE_FROM_CLAIMED_ORIGIN = 30;
+    const MAX_DISTANCE_FROM_CLAIMED_DESTINATION = 30;
     public static function updateStatus($trip_id, $status):array{
-        $trip = new Trip();
-        $trip = $trip->getById($trip_id);
-        if ($trip == null) {
-            throw new \Exception("Trip not found", 404);
-        }
+        $trip = Trip::getById_Or404($trip_id);
+
         $priority = Trip::$fieldRules['status']['values'];
         if (array_search($status, $priority) < array_search($trip->get_status(), $priority)) {
             throw new \Exception("Already passed", 409);
         }
+
         if ($trip->get_biker_id() == null) {
             throw new \Exception("Biker is not assigned", 409);
         }
-        $biker = new Biker();
-        $biker = $biker->getById($trip->get_biker_id());
-        if ($status == 'picked' && Location::Distance($biker->get_location()['latitude'], $biker->get_location()['longitude'], $trip->get_origin()['latitude'], $trip->get_origin()['longitude']) > 30) {
+
+        $biker = Biker::getById_Or404($trip->get_biker_id());
+        $bikerLatitude = $biker->get_location()['latitude'];
+        $bikerLongitude = $biker->get_location()['longitude'];
+        $originLatitude = $trip->get_origin()['latitude'];
+        $originLongitude = $trip->get_origin()['longitude'];
+        $destinationLatitude = $trip->get_destination()['latitude'];
+        $destinationLongitude = $trip->get_destination()['longitude'];
+        if (
+            $status == 'picked' &&
+            Location::Distance(
+                $bikerLatitude,
+                $bikerLongitude,
+                $originLatitude,
+                $originLongitude) > self::MAX_DISTANCE_FROM_CLAIMED_ORIGIN) {
             throw new \Exception("Biker is too far from origin", 409);
         }
-        if ($status == 'delivered' && Location::Distance($biker->get_location()['latitude'], $biker->get_location()['longitude'], $trip->get_destination()['latitude'], $trip->get_destination()['longitude']) > 30) {
+        if (
+            $status == 'delivered' &&
+            Location::Distance(
+                $bikerLatitude,
+                $bikerLongitude,
+                $destinationLatitude,
+                $destinationLongitude) > self::MAX_DISTANCE_FROM_CLAIMED_DESTINATION) {
             throw new \Exception("Biker is too far from destination", 409);
         }
 
         $trip->insertData([
             'status' => $status
-        ], Trip::$fieldRules);
-        try {
-            $trip->save();
-        }
-        catch (\Exception $e) {
-            throw new \Exception("Failed to update status", 500);
-        }
+        ]);
+        $trip->save();
         return $trip->exportData();
     }
 
