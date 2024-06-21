bf037efbe676d31de844ce5cd4c489c0ae7d2f3d
erfan.gooneh@zoodfood.ir <erfan.gooneh@zoodfood.ir>
2024-06-21 21:46:28 +0330

    Cleaning Vendor Services and Controllers

--- src/Controllers/VendorController.php
+++ src/Controllers/VendorController.php
@@ -6,58 +6,70 @@ use App\Services\VendorService;
 use App\Models\Vendor;
 use App\Errors;
 use App\Field;
+
 class VendorController{
     public function get($params):void{
         if($_SERVER['REQUEST_METHOD'] == 'GET') {
-            try {
-                $vendor_id = $params['vendorId'];
-                $vendor_field = new Field(Vendor::$fieldRules['id'], $vendor_id, Vendor::class, 'id');
-                $vendor_field->validate();
-            }
-            catch (\Throwable $e) {
-                Errors::InvalidInput($e->getMessage());
-            }
-            try {
+            $vendor_id = $this->tryToGetVendorId($params);
+            $serialized_vendor = $this->tryToGetVendor($vendor_id);
+            Response::JsonResponse($serialized_vendor);
+        }
+    }
 
-                $res = VendorService::getVendor($vendor_id);
-                Response::JsonResponse($res);
-            } catch (\Exception $e) {
-                if ($e->getCode() == 404) {
-                    Errors::NotFound($e->getMessage());
-                } else {
-                    Errors::ServerError($e->getMessage());
-                }
+    private function tryToGetVendorId($params) {
+        try{
+            $vendor_id = $params['vendorId'];
+            return $vendor_id;
+        }
+        catch (\Throwable $e) {
+            Errors::InvalidInput($e->getMessage());
+        }
+    }
+
+    private function tryToGetVendor($vendor_id) {
+        try {
+            return VendorService::getVendor($vendor_id);
+        }
+        catch (\Exception $e) {
+            if ($e->getCode() == 404) {
+                Errors::NotFound($e->getMessage());
+            } else {
+                Errors::ServerError($e->getMessage());
             }
         }
     }
+
     public function create():void{
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            try{
-                $data = json_decode(file_get_contents('php://input'), true);
-                $name = $data['name'];
-                $latitude = $data['latitude'];
-                $longitude = $data['longitude'];
-                $vendor_name_field = new Field(Vendor::$fieldRules['name'], $name, Vendor::class, 'name');
-                $vendor_longitude_field = new Field(Vendor::$fieldRules['longitude'], $longitude, Vendor::class, 'longitude');
-                $vendor_latitude_field = new Field(Vendor::$fieldRules['latitude'], $latitude, Vendor::class, 'latitude');
-                $vendor_name_field->validate();
-                $vendor_latitude_field->validate();
-                $vendor_longitude_field->validate();
-            }
-            catch(\Throwable $e){
+            list($name, $latitude, $longitude) = $this->tryToGetVendorDataFromInput();
+            $serialized_created_vendor = $this->tryToCreateVendor($name, $latitude, $longitude);
+            Response::JsonResponse($serialized_created_vendor, 201);
+        }
+    }
+
+    private function tryToGetVendorDataFromInput() {
+        try{
+            $data = json_decode(file_get_contents('php://input'), true);
+            $name = $data['name'];
+            $latitude = $data['latitude'];
+            $longitude = $data['longitude'];
+            return [$name, $latitude, $longitude];
+        }
+        catch(\Throwable $e){
+            Errors::InvalidInput($e->getMessage());
+        }
+    }
+
+    private function tryToCreateVendor($name, $latitude, $longitude) {
+        try{
+            return VendorService::createVendor($name, $latitude, $longitude);
+        }
+        catch(\Exception $e){
+            if($e->getCode() == 400){
                 Errors::InvalidInput($e->getMessage());
             }
-            try{
-                $res = VendorService::createVendor($name, $latitude, $longitude);
-                Response::JsonResponse($res, 201);
-            }
-            catch(\Exception $e){
-                if($e->getCode() == 400){
-                    Errors::InvalidInput($e->getMessage());
-                }
-                else{
-                    Errors::ServerError($e->getMessage());
-                }
+            else{
+                Errors::ServerError($e->getMessage());
             }
         }
     }
--- src/Services/VendorService.php
+++ src/Services/VendorService.php
@@ -6,32 +6,17 @@ use App\Models\Vendor;
 
 class VendorService{
     public static function getVendor($vendor_id):array{
-        $vendor = new Vendor();
-        $vendor = $vendor->getById($vendor_id);
-        if ($vendor == null) {
-            throw new \Exception("Vendor not found", 404);
-        }
+        $vendor = Vendor::getById_Or404($vendor_id);
         return $vendor->exportData();
     }
     public static function createVendor(string $name, $latitude, $longitude):array{
         $vendor = new Vendor();
-        try{
-            $vendor->insertData([
-                'name'=> $name,
-                'latitude' => $latitude,
-                'longitude' => $longitude
-            ], Vendor::$fieldRules);
-        }
-        catch (\Exception $e) {
-            throw new \Exception($e->getMessage(), 400);
-        }
-
-        try {
-            $vendor->create();
-        }
-        catch (\Exception $e) {
-            throw new \Exception("Failed to create vendor", 500);
-        }
+        $vendor->insertData([
+            'name'=> $name,
+            'latitude' => $latitude,
+            'longitude' => $longitude
+        ]);
+        $vendor->create();
         return $vendor->exportData();
     }
 }
